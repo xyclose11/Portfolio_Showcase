9393f726dc7bc4af897524330a16b962f6abb583
xyclose11 <harrisgus1@hotmail.com>
2025-03-06 10:27:06 -0600

    Enhance project card layout and styles; update project descriptions and technologies for clarity and consistency

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -13,32 +13,63 @@ export class ProjectsComponent {
     {
       title: 'Melin',
       description:
-        'An academic reference management applcaiton that allows users to create, edit, and delete references. Implemented PDF, and JSON parsing, alongside a custom barcode implementation that allows users to automatically scan a barcode and upload the reference data, using the mobile application. Also implemented sorting, filtering, tagging, grouping, user auth and searching functionality.',
+        'An academic reference management application, designed to help users manage and organize their academic references efficiently.',
       images: ['images/Melin.png'],
-      technologies: ['Angular', 'Ionic', 'Firebase'],
+      technologies: [
+        'ASP.NET Core',
+        'C#',
+        'React.js',
+        'TypeScript',
+        'Jenkins',
+        'PostgreSQL',
+        'HTML/CSS',
+        'Docker',
+        'Nginx',
+        'xUnit',
+      ],
       github: 'https://github.com/xyclose11/Melin',
     },
     {
       title: 'MESS',
-      description: 'This is the description of project 2',
+      description:
+        'Engineered a Manufacturing Execution System Software for a local manufacturing company, enabling real-time tracking and analysis of product progress through the manufacturing process.',
       images: ['images/ProductionLogFailure.png'],
-      technologies: ['Angular', 'Ionic', 'Firebase'],
+      technologies: [
+        'ASP.NET Core',
+        'C#',
+        'Blazor',
+        'MSSQL',
+        'JavaScript',
+        'EF Core',
+        'Jenkings',
+        'Nginx',
+        'Visual Studio',
+      ],
       github: 'https://github.com/SensitTechnologies/MESS',
     },
     {
-      title: 'Project 3',
-      description: 'This is the description of project 3',
+      title: 'Citation Generator',
+      description:
+        'Built a student facing citation generation full-stack web application, to import, export, and generate citations securely.',
       images: [
         'https://placehold.co/600x400/EEE/31343C?font=montserrat&text=Images Coming Soon...',
       ],
-      technologies: ['Angular', 'Ionic', 'Firebase'],
+      technologies: [
+        'TypeScript',
+        'Node.js',
+        'HTML/CSS',
+        'Next.js',
+        'MongoDB',
+        'Apache',
+      ],
       github: 'https://github.com/xyclose11/cashmoneycitations',
     },
     {
-      title: 'Project 3',
-      description: 'This is the description of project 3',
+      title: 'Paint',
+      description:
+        'Created a custom image manipulation tool based on the popular Microsoft Paint.',
       images: ['images/Rotate.png'],
-      technologies: ['Angular', 'Ionic', 'Firebase'],
+      technologies: ['Java', 'JavaFX', 'JUnit', 'UML', 'Maven'],
       github: 'https://github.com/xyclose11/Paint',
     },
   ];
